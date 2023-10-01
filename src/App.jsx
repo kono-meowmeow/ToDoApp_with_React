ac47c5eb09d1cd1da35eea76cc7d02192725080d
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-10-01 11:26:44 +0900

    mapを使ったループ処理への変更

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = React.useState(["ああああ", "いいいい"]);
+  const [completeTodos, setCompleteTodos] = React.useState(["うううう"]);
   return (
     <>
       {/* jsxではclass名を与えるのに、classNameを使う点に注意 */}
@@ -12,24 +14,39 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p>ああああ</p>
-              <button>完了</button>
-              <button>削除</button>
-            </div>
-          </li>
+          {/* map関数を使って、配列の要素を一つずつ取り出す */}
+          {/* この場合、incompleteTodosの要素を一つずつ取り出している */}
+          {/* そのため、incompleteTodosの要素の数だけli要素が生成される */}
+          {incompleteTodos.map((todo) => {
+            return(
+              // map関数の中でreturnを使う場合、keyを設定する必要がある
+              // このkeyは、Reactが仮想DOM内で要素を識別するためのもの
+              // このkeyは、一意である必要がある
+              // そのため、ここではtodoをkeyとしている
+              <li key={todo} >
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>完了</button>
+                  <button>削除</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="complete-area">
         <p className="title">完了のTODO</p>
         <ul>
-          <li>
-            <div className="list-row">
-              <p>ああああ</p>
-              <button>戻す</button>
-            </div>
-          </li>
+          {completeTodos.map((todo) => {
+            return(
+              <li key={todo}>
+                <div className="list-row">
+                  <p>{todo}</p>
+                  <button>戻す</button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
